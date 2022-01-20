d96e83220ee7065afc7e97d521f602c9d7c7b3cb
adityagesh <adityagesh@gmail.com>
2022-01-20 19:45:10 +0530

    feat: Method to add headers to requests fixes #2

--- postmanrenderer/main.py
+++ postmanrenderer/main.py
@@ -2,63 +2,74 @@ from constants import APP, HTTP_METHOD, POSTMAN
 import uuid
 from jinja2 import Template, FileSystemLoader, Environment
 from typing import List
-import jinja_env 
+import jinja_env
 
-# Representation of Header of a Request 
+# Representation of Header of a Request
+
+
 class Header:
     def __init__(self):
         pass
 
 # Representation of Request Url
-class Url: 
+
+
+class Url:
     def __init__(self, url: str):
         url_split = url.split("://")
         self.protocol = url_split[0] if len(url_split) > 1 else None
         self.raw = url_split[-1]
         self.host = self.raw.split(".")
 
+
 # Representation of Request in a Collection
 class Request:
-    def __init__(self, name: str, method: HTTP_METHOD, description: str, headers: List[Header], url: Url) -> None:
+    def __init__(self, name: str, method: HTTP_METHOD, description: str, url: Url) -> None:
         self.name = name
         self.method = method
         self.description = description
-        self.headers = headers
+        self.headers = dict()
         self.url = url
+
+    def add_header(self, key: str, value: str):
+        self.headers[key] = value
+
 
 # Representation of Postman Collection
 class Collection:
-    def __init__(self, name, id = None) -> None:
+    def __init__(self, name, id=None) -> None:
         self.id = uuid.uuid4() if id == None else id
         self.name = name
         self.schema = POSTMAN.schema
         self.requests = []
-        
+
     def add_request(self, request: Request):
         self.requests.append(request)
-    
-    
+
     def get_template_object(self) -> Template:
         file_loader = FileSystemLoader(APP.root_dir + APP.template_dir)
         env = Environment(loader=file_loader)
         jinja_env.init(env)
         template = env.get_template(APP.collections_template)
         return template
-    
+
     def render(self, template: Template) -> str:
-        rendered_template = template.render(collection= self)
+        rendered_template = template.render(collection=self)
         return rendered_template
-        
-    
+
+
 if __name__ == "__main__":
-   collection = Collection("sample_collection")
-   request = Request("Yahoo request", HTTP_METHOD.GET,"", [], Url("https://www.yahoo.com"))
-   collection.add_request(request)
-   
-   request = Request("Google request", HTTP_METHOD.GET,"", [], Url("https://www.google.com"))
-   collection.add_request(request)
-   
-   template = collection.get_template_object()
-   render = collection.render(template)
-   print(render)
-   
+    collection = Collection("sample_collection")
+    request = Request("Yahoo request", HTTP_METHOD.GET,
+                      "", Url("https://www.yahoo.com"))
+    collection.add_request(request)
+
+    request = Request("Google request", HTTP_METHOD.GET,
+                      "", Url("https://www.google.com"))
+    request.add_header("Content-Type", "Application/json")
+
+    collection.add_request(request)
+
+    template = collection.get_template_object()
+    render = collection.render(template)
+    print(render)
